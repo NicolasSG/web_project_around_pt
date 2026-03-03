67a430233b86d0cc8b0fa76d20a3f71cd1938598
Nicolas <neco.beatles@gmail.com>
2026-03-03 16:59:18 -0300

    js array / forEach

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+let initialCards = [
+  ["Vale de Yosemite", "https://code.s3.yandex.net/web-code/yosemite.jpg"],
+  ["Lago Louise", "https://code.s3.yandex.net/web-code/lake-louise.jpg"],
+  [
+    "Montanhas Carecas",
+    "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  ],
+  ["Latemar", "https://code.s3.yandex.net/web-code/latemar.jpg"],
+  [
+    "Parque Nacional da Vanoise",
+    "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  ],
+  ["Lago di Braies", "https://code.s3.yandex.net/web-code/lago.jpg"],
+];
+
+initialCards.forEach(function (item) {
+  console.log(item);
+});
